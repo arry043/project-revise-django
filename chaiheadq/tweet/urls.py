b80d5b1878ced2734bfe6c131956eaceca2062b6
Arry <arifquerry@gmail.com>
2026-01-18 16:03:44 +0530

    add

--- chaiheadq/tweet/urls.py
+++ chaiheadq/tweet/urls.py
@@ -2,5 +2,9 @@ from django.urls import path, include
 from tweet import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', views.tweet_list, name='tweet_list'),
+    path('tweet_create/', views.tweet_create, name='tweet_create'),
+    path('<int:tweet_id>/tweet_edit/', views.tweet_edit, name='tweet_edit'),
+    path('<int:tweet_id>/tweet_delete/', views.tweet_delete, name='tweet_delete'),
 ]
