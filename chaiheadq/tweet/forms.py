934be4fb564bf494b2c5c04fe2a867451f280db2
Arry <arifquerry@gmail.com>
2026-01-19 16:23:09 +0530

    db

--- chaiheadq/tweet/forms.py
+++ chaiheadq/tweet/forms.py
@@ -4,4 +4,15 @@ from .models import Tweet
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
-        fields = ('text', 'photo')
+        fields = ['text', 'photo']
+
+        widgets = {
+            'text': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'What’s happening, Arry? 😎'
+            }),
+            'photo': forms.ClearableFileInput(attrs={
+                'class': 'form-control'
+            })
+        }
